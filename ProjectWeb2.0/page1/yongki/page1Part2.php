954f536539d6fcccd3958eae84c7eb45463b70e0
fabiansuryajayas <54829878+fabiansuryajayas@users.noreply.github.com>
2019-10-26 20:25:58 +0700

    footer

--- ProjectWeb2.0/page1/yongki/page1Part2.php
+++ ProjectWeb2.0/page1/yongki/page1Part2.php
@@ -1,30 +1,14 @@
 <link rel="stylesheet" href="yongki/stylePage1Part2.css">
 <div class="container">
-    <div class="col-bootstrap col-md-6" style="float:left;">
+    <div class="col-bootstrap col-md-6">
         <div class="whySTTS">WHY STTS</div>
-        <h2 class="section-title text-danger" style="margin-top:100px;">
-            Pursue Your Passion & Purpose Here<span class="text-primary">.</span>
-        </h2>
-        <span>#UPHIMPACTSLIVES</span> <br> <br>
-
-        <div class="card" style="max-width: 450px;">
-            <div class="row no-gutters">
-                <div class="col-md-4">
-                    <img src="../assets/test2.jpg" class="card-img-top h-100" alt="...">
-                </div>
-                <div class="col-md-6">
-                    <div class="card-body">
-                        <span class="card-title text-primary">STUDENT ACTIVITIES</span>
-                        <p class="card-text">BE A PART OF OUR COMMUNITY</p>
-                        <a href="card-link">-READ MORE</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        
-        
+        Pursue Your Passion & Purpose Here.
+        #UPHIMPACTSLIVES
+        <img src="../assets/test2.jpg" alt="">
+        STUDENT ACTIVITIES <br>
+        BE A PART OF OUR COMMUNITY<br>
     </div>
-    <div class="col-bootstrap col-md-4" style="float:left;">
+    <div class="col-bootstrap col-md-6">
         <img src="../assets/test1.jpg" alt="">
     </div>
 </div>
