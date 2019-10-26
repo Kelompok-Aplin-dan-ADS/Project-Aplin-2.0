f2224fb9c065648a30d94067e54bd366c9ae2998
Yongki <yongkitanu@gmail.com>
2019-10-26 18:11:54 +0700

    ada folder page1 dan yongki
    nanti yang dijalankan index.php nya
    belum connect connection nya

---
+++ ProjectWeb2.0/page1/yongki/page1Part2.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" href="yongki/stylePage1Part2.css">
+<div class="container">
+    <div class="col-bootstrap col-md-6">
+        <div class="whySTTS">WHY STTS</div>
+        Pursue Your Passion & Purpose Here.
+        #UPHIMPACTSLIVES
+        <img src="../assets/test2.jpg" alt="">
+        STUDENT ACTIVITIES <br>
+        BE A PART OF OUR COMMUNITY<br>
+    </div>
+    <div class="col-bootstrap col-md-6">
+        <img src="../assets/test1.jpg" alt="">
+    </div>
+</div>
--- ProjectWeb2.0/page1Part2.php
+++ ProjectWeb2.0/page1Part2.php
@@ -4,11 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>ISTTS</title>
-    <link rel="stylesheet" href="../bootstrap4/css/bootstrap.min.css">
+    <title>Document</title>
 </head>
 <body>
-    <p class="font-weight-bold">Bold text.</p>
-    <script src="../bootstrap4/js/bootstrap.min.js"></script>
+    
 </body>
 </html>
