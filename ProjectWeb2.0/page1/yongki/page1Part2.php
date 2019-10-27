55074d54cc06ea281c5622f41c13d51ffdcede90
Yongki <yongkitanu@gmail.com>
2019-10-27 10:29:26 +0700

    page1part2 yongki sudah selesai

--- ProjectWeb2.0/page1/yongki/page1Part2.php
+++ ProjectWeb2.0/page1/yongki/page1Part2.php
@@ -2,12 +2,12 @@
 <div class="container">
     <div class="col-bootstrap col-md-6" style="float:left;">
         <div class="whySTTS">WHY STTS</div>
-        <h2 class="section-title text-danger" style="margin-top:100px;">
+        <h1 class="section-title text-danger" style="margin-top:100px;">
             Pursue Your Passion & Purpose Here<span class="text-primary">.</span>
         </h2>
         <span>#UPHIMPACTSLIVES</span> <br> <br>
 
-        <div class="card hoverCard" style="max-width: 450px;">
+        <div class="card hoverCard rounded" style="max-width: 450px;">
             <div class="row no-gutters">
                 <div class="col-md-4">
                     <img src="../assets/test2.jpg" class="card-img-top h-100" alt="...">
@@ -33,14 +33,39 @@
         </div>
     </div>
 
-    <div class="col-bootstrap col-md-4" style="float:left; margin-top:100px; margin-left:30px;">
-        <img src="../assets/test1.jpg" class="hoverCard" style="width:450px; "alt="">
+    <div class="col-bootstrap col-md-5" style="float:left; margin-top:100px; margin-left:30px;">
+        <img src="../assets/test1.jpg" class="hoverCard" style="width:450px; height:300px; "alt="">
         <br> <br>
-        <div class="card">
-            <div class="card-image">
-                <img src="../assets/gedung.jpg" alt="">
+        <div class="card hoverCard" style="max-width: 450px;">
+            <div class="row no-gutters card-img-right">
+                <div class="col-md-5">
+                    <img src="../assets/card3.jpg" class="card-img-top h-100" alt="...">
+                </div>
+                <div class="col-md-7">
+                    <div class="card-body">
+                        <span class="card-title text-primary">STUDENT ACTIVITIES</span>
+                        <p class="card-text">BE A PART OF OUR COMMUNITY</p>
+                        <a href="card-link">-READ MORE</a>
+                    </div>
+                </div>
             </div>
         </div>
+        <br>
 
+        <div class="card hoverCard" style="max-width: 450px;">
+            <div class="row no-gutters card-img-right">
+                <div class="col-md-5">
+                    <img src="../assets/card4.jpg" class="card-img-top h-100" alt="...">
+                </div>
+                <div class="col-md-7">
+                    <div class="card-body">
+                        <span class="card-title text-primary">STUDENT ACTIVITIES</span>
+                        <p class="card-text">BE A PART OF OUR COMMUNITY</p>
+                        <a href="card-link">-READ MORE</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br>
     </div>
 </div>
