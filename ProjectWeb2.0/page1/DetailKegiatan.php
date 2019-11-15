255fd5a775ad14f7c3dde023ef4d9076f47e8e66
fabiansuryajaya <54833728+fabiansuryajaya@users.noreply.github.com>
2019-11-15 11:44:49 +0700

    Create DetailKegiatan.php

---
+++ ProjectWeb2.0/page1/DetailKegiatan.php
@@ -0,0 +1,21 @@
+<?php
+    include_once("..//connection.php");
+    $id=5;
+    $semuaAcara=mysqli_fetch_assoc(mysqli_query($conn,"SELECT * from acara where id_acara=$id"));
+    
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <img src=".<?=$semuaAcara["gambar"]?>" alt="">
+    <div><?php echo $semuaAcara["tempat"]." ".$semuaAcara["waktu"]?></div>
+    <div><?php echo $semuaAcara["judul"]?></div>
+    <div><?php echo $semuaAcara["deskripsi"]?></div>
+</body>
+</html>
