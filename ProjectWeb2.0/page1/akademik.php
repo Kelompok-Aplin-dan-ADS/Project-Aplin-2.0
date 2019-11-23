bd9b9adc3abd684fdbb247d821609003c17accf3
fabiansuryajaya <54833728+fabiansuryajaya@users.noreply.github.com>
2019-11-23 20:28:33 +0700

    ajax

--- ProjectWeb2.0/page1/akademik.php
+++ ProjectWeb2.0/page1/akademik.php
@@ -75,6 +75,18 @@
 <body>
     <div class="kiri" >
         <div class="kiri-atas">AKADEMI</div>
+        <div>
+            <span onclick="panggilAkademik(1)">d3 informatika</span> <br>
+            <span onclick="panggilAkademik(11)">s1 informatika</span> <br>
+            <span onclick="panggilAkademik(14)">s1 informatika profesional</span> <br>
+            <span onclick="panggilAkademik(14)">s1 sib</span> <br>
+            <span onclick="panggilAkademik(14)">s1 industri</span> <br>
+            <span onclick="panggilAkademik(10)">s1 elektro</span> <br>
+            <span onclick="panggilAkademik(14)">s1 dkv</span> <br>
+            <span onclick="panggilAkademik(14)">s1 despro
+
+
+        </div>
     </div>
     <div class="tengah">
         <ul class="ajaxnya">
--- ProjectWeb2.0/page1/connection.php
+++ ProjectWeb2.0/page1/connection.php
@@ -4,6 +4,7 @@
     $pass = "";
     $dbBase = "project-aplin-2.0";
     $dbBase2 = "sttsedunew";
+    
     $conn = mysqli_connect($host,$user,$pass,$dbBase);
     $connStts = mysqli_connect($host,$user,$pass,$dbBase2);
 ?>
