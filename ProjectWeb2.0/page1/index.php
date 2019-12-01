25bcabb8b72accc744bc3ce4d735973593f4fb36
fabiansuryajayas <54829878+fabiansuryajayas@users.noreply.github.com>
2019-12-01 15:33:32 +0700

    kegiatan

--- ProjectWeb2.0/page1/index.php
+++ ProjectWeb2.0/page1/index.php
@@ -10,12 +10,10 @@
 </head>
 <body>
 	<?php include('fabian/navbar.php') ?>
-    <div class="content-isi">
         <?php include('fabian/caurousel.php') ?>
         <?php include('yongki/page1Part2.php') ?>
-        <?php include('samuel/discover-2Ralat.php')?>
+        <?php include('discover-2Ralat.php')?>
         <?php include('Ming/calJur.php') ?>
-    </div>
 	<?php include('fabian/footer.php') ?>
     <script src="../bootstrap4/js/js.js"></script>
     <script src="../bootstrap4/js/proper.js"></script>
