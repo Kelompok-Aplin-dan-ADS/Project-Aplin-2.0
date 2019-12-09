65fb0d5b6f1399ceb70ca24acddef18065aa6823
zinu825 <54827064+zinu825@users.noreply.github.com>
2019-12-09 22:31:35 +0700

    nambah i bahasa dan betulkan tag

--- ProjectWeb2.0/page1/DetailKegiatan.php
+++ ProjectWeb2.0/page1/DetailKegiatan.php
@@ -1,9 +1,20 @@
+<?php  
+    session_start();
+    $bahasa = 1;
+    if (isset($_SESSION['bahasa'])) {
+        $bahasa = $_SESSION['bahasa'];
+    }
+    else{
+        $bahasa = 1; 
+    }
+?>
 <?php
     include_once("connection.php");
     $id=$_GET["acara"];
     $semuaAcara=mysqli_fetch_assoc(mysqli_query($conn,"SELECT * from acara where id_acara=$id"));
     $query_tag =mysqli_fetch_array(mysqli_query($conn,"SELECT acara.tag FROM acara WHERE id_acara='$id'"));        
-    $splittag = explode("|",$query_tag["tag"]);    
+    $splittag = explode("|",$query_tag["tag"]); 
+    $query_nama_tag = mysqli_query($conn, "SELECT * FROM tag");    
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -18,13 +29,18 @@
     <div>
         <?php 
             $index = count($splittag);
-            for ($i=0; $i < $index; $i++) { 
-                echo "<button type='button' disabled='false' class='btn btn-primary'>".$splittag[$i]."</button>";
+            while ($row = mysqli_fetch_array($query_nama_tag)) {
+                for ($i=0; $i < $index; $i++) { 
+                    if ($row['tag_id'] == $splittag[$i]) {                        
+                        echo "<button type='button' disabled='false' class='btn btn-primary'>".$row['tag_nama_'.$bahasa]."</button>";
+                    }                        
+                }            
             }
+            
         ?>            
     </div>
     <div><?php echo $semuaAcara["tempat"]." ".$semuaAcara["waktu"]?></div>
-    <div><?php echo $semuaAcara["judul"]?></div>
-    <div><?php echo $semuaAcara["deskripsi"]?></div>
+    <div><?php echo $semuaAcara["judul_".$bahasa]?></div>
+    <div><?php echo $semuaAcara["deskripsi_".$bahasa]?></div>
 </body>
 </html>
