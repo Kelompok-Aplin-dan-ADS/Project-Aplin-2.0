a53eaa166dcd5ff480c56b22188c4bcf44a8ceb6
zinu825 <54827064+zinu825@users.noreply.github.com>
2019-12-18 23:44:36 +0700

    meida massa

--- ProjectWeb2.0/page1/media_massa.php
+++ ProjectWeb2.0/page1/media_massa.php
@@ -13,11 +13,42 @@
   }
 ?>    
   <style>
+    #bagian-berita{
+      position: absolute;      
+      top: 50%;
+      left: 50%;
+      transform: translateX(-50%) translateY(-50%) ;
+    }
+   #menu-1 , #menu-2 ,#menu-3{
+      display: inline-block;
+    }
+    #menu-1 a, #menu-2 a , #menu-3 a{
+      text-decoration: none;
+      display: inline-block;
+      color: black;
+      font-family: arial;
+      font-weight: lighter;
+      font-size: 40px;
+      padding: 0 28px;
+    }
+    .cool-link::after{
+      content: '';
+      display: block;
+      width: 0px;
+      height: 2px;
+      border: 5px solid white;    
+      transition: width .3s;
+    }
+    .cool-link:hover::after{
+      border: 5px solid black;            
+      width: 100%;        
+      transition: width .3s;        
+    }
   #bagian-media-massa{    
     height: 99vh;
     width: 100%;
     position: relative;
-    margin-top: 10%;        
+    margin-top: 12%;        
   }  
   * {   
    font-family: sans-serif;
@@ -30,31 +61,28 @@
     border-radius: 3%;
     z-index: 5;            
   }
+  #content-media{
+    /*padding-bottom: 10px;*/
+  }
   </style>  
   <div class="container">
   <div class="col-md-12" id="bagian-media-massa" >
     <div class="row">   
       <div class="col-md-8">
         <div class="container" id="bagian-berita">
-          <ul class="nav nav-tabs">
-            <li class="nav-item" class="tekan">
-              <a class="nav-link active" onclick="pilihacara(1)"><p>Berita</p></a>
-            </li>
-            <li class="nav-item" class="tekan">
-              <a class="nav-link" onclick="pilihacara(2)"><p>Agenda</p></a>
-            </li>          
-          </ul>       
+          <li id="menu-2"><a onclick="pilihacara(1)" class="cool-link">Berita</a></li>
+          <li id="menu-1"><a onclick="pilihacara(2)" class="cool-link">Agenda</a></li>          
           <div id="content-berita-agenda"></div>   
         </div>
       </div>  
       <div class="col-md-4">
         <div id="content-media">
-          <h2>MEDIA MASSA</h2>
+          <li id="menu-3"><a class="cool-link">MEDIA MASSA</a></li>
           <?php
             $query = mysqli_query($conn, "SELECT * FROM acara WHERE kategori ='2'");
             while ($row = mysqli_fetch_array($query)) {
               echo "<div id='isi'>
-  <a href='DetailKegiatan.php?acara=".$row['id_acara']."'>".$row['judul_'.$bahasa]."</a><p>".substr($row['deskripsi_'.$bahasa],0,32)."</p></div>";            
+  <a href='Fabian/DetailKegiatan.php?acara=".$row['id_acara']."'>".$row['judul_'.$bahasa]."</a><p>".substr($row['deskripsi_'.$bahasa],0,32)."</p></div>";            
             }
           ?>
         </div>
@@ -62,12 +90,7 @@
     </div>    
   </div>    
   </div>
-  <script>    
-    // $(document).ready(function(){
-    //   $(".tekan").show("slow",1000 , function(){
-    //     pilihacara(ind);
-    //   });
-    // });
+  <script>        
     function pilihacara(ind){    
       $.post("ajax_media.php",{mode: "media",t1: ind},
         function(result){                                
